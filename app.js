0bf13f827e12690fdb27a744099e4e32f0a26bbb
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-11-27 16:48:42 +0530

    Add package.json with initial dependencies for cat-me and express

---
+++ app.js
@@ -0,0 +1,17 @@
+// const catMe = require("cat-me")
+// console.log(catMe())
+
+// const http = require("http")
+// const server = http.createServer((req, res)=>{
+//     if(req.url == "/about"){
+//        return res.end("this is about page")
+//     }
+//     if(req.url == "/profile"){
+//        return res.end("this is profile page")
+//     }
+//     if(req.url == "/"){
+//        return res.end("this is home page")
+//     }
+//     res.end("404 page not found")
+// })
+// server.listen(3000)
