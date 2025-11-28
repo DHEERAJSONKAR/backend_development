3b2a6572a0b56dd6c9f5fc30c5ba8bc2ebab36f2
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-11-28 23:44:07 +0530

    Add safe-buffer package and EJS view template
    - Introduced the safe-buffer package to provide a safer Node.js Buffer API.
    - Added LICENSE, README.md, index.d.ts, index.js, and package.json files for safe-buffer.
    - Created an EJS view template (index.ejs) with basic HTML structure and content.

--- temp.js
+++ temp.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res)=>{
-    res.send("Director Dheeraj");
+app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+    console.log("This is middleware");
+    const a=10
+    const b=20
+    const sum=a+b
+    console.log("Sum is: "+sum);
+    return next();
 })
-app.get("/about", (req, res)=>{
+app.get('/', (req, res) => {
+    res.render("index");
+})
+app.get("/about", (req, res) => {
     res.send("This is about page");
 })
-app.get("/profile", (req, res)=>{
+app.get("/profile", (req, res) => {
     res.send("This is profile page");
 })
 app.listen(3000);
