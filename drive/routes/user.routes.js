4f1ab15eb4cf657e3daf782c668cd7f03ca247d2
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-12-02 22:50:59 +0530

    Initialize project structure with Express, EJS views, and user registration form

---
+++ drive/routes/user.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
+router.post('/register', (req,res)=>{
+    console.log(req.body);
+    res.send('Registration successful');
+})
+
+
+
+module.exports = router;
