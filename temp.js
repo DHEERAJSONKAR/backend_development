d8d429b40088a942e8f6d77c3d4bc76f84370f50
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-12-02 19:13:26 +0530

    02/12/2025
    connection with mongodb

--- temp.js
+++ temp.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
+const dbConnection = require('./config/db');
+const userModel = require('./models/user');
 const app = express();
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }));
@@ -18,5 +20,38 @@ app.get("/profile", (req, res) => {
 app.post("/get-form-data",(req,res)=>{
     console.log(req.body);
     res.send("Form data received");
+})
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+app.post("/register", async(req,res)=>{
+   const {username, email, password} = req.body; 
+   const newUser = await userModel.create({
+    username: username,
+    email: email,
+    password: password
+})
+    res.send(newUser);
+})
+app.get("/get-user", (req,res)=>{
+    userModel.findOne({
+        username:"dheeraj"
+    }).then((users)=>{
+        res.send(users);
+    })
+})
+app.get("/update-user", async(req,res)=>{
+    await userModel.findOneAndUpdate({
+        username:"dheeraj"
+    },{
+        email:"raj23@gmail.com"
+    })
+    res.send("User Updated")
+})
+app.get("/delete-user", async(req,res)=>{
+    await userModel.findOneAndDelete({
+        username:"dheeraj"
+    })
+    res.send("User Deleted")
 })
 app.listen(3000);
